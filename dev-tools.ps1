72dc84deea99e5f64b8f2537d0315c80842933da
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2023-09-20 15:57:42 +0200

    Enabled tailwind watch

---
+++ dev-tools.ps1
@@ -0,0 +1,2 @@
+# This script runs all tools that are needed during development
+.\dev\tailwindcss-windows-x64.exe -i ".\src\BlazorHeightmaps.Wasm.App\wwwroot\css\app.css" -o ".\src\BlazorHeightmaps.Wasm.App\wwwroot\css\app.min.css" --watch
