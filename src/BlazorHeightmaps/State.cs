47eb3696d1dc20fc900e8b11024327ed8c6a2627
AmbachtIT <34813625+AmbachtIT@users.noreply.github.com>
2023-10-12 09:27:07 +0200

    Added state

---
+++ src/BlazorHeightmaps/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ambacht.Common.Maps;
+
+namespace BlazorHeightmaps
+{
+	public class State
+	{
+
+		public LatLng Center { get; set; }
+
+		public int ZoomLevel { get; set; }
+
+	}
+}
